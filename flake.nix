4d41b35b2a07fa1155eb6b422d5aaa551278b921
Alex Martens <alex@thinglab.org>
2022-11-11 11:53:35 -0800

    Port to nix

---
+++ flake.nix
@@ -0,0 +1,103 @@
+{
+  description = "WebUI for brother QL label printers";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pyproject = nixpkgs.lib.importTOML ./pyproject.toml;
+    pname = pyproject.tool.poetry.name;
+
+    python3Overlay = final: prev:
+      prev.buildPythonPackage {
+        inherit pname;
+        inherit (pyproject.tool.poetry) version;
+        format = "pyproject";
+
+        src = ./.;
+
+        nativeBuildInputs = [
+          prev.poetry-core
+        ];
+
+        propagatedBuildInputs = [
+          prev.brother-ql
+          prev.bottle
+          prev.jinja2
+        ];
+
+        pythonImportsCheck = [
+          pname
+        ];
+
+        meta = with nixpkgs.lib; {
+          inherit (pyproject.tool.poetry) description;
+          homepage = pyproject.tool.poetry.repository;
+          license = with licenses; [gpl3];
+        };
+      };
+
+    overlay = final: prev: rec {
+      python3 = prev.python3.override {
+        packageOverrides = final: prev: {
+          brother_ql_web = python3Overlay final prev;
+        };
+      };
+      python3Packages = python3.pkgs;
+    };
+
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [overlay];
+    };
+  in {
+    overlays = {
+      default = overlay;
+      python3 = python3Overlay;
+    };
+
+    formatter.x86_64-linux = pkgs.alejandra;
+
+    packages.x86_64-linux.default = pkgs.python3Packages.brother_ql_web;
+
+    nixosModules.default = import ./module.nix;
+
+    checks.x86_64-linux = let
+      nixSrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".nix"];
+      pySrc = nixpkgs.lib.sources.sourceFilesBySuffices ./. [".py"];
+    in {
+      pkg = self.packages.x86_64-linux.default;
+
+      black = pkgs.runCommand "black" {} ''
+        ${pkgs.python3Packages.black}/bin/black --config ${./pyproject.toml} ${pySrc}
+        touch $out
+      '';
+
+      flake8 =
+        pkgs.runCommand "flake8"
+        {
+          buildInputs = with pkgs.python3Packages; [
+            flake8
+            flake8-bugbear
+            pep8-naming
+          ];
+        }
+        ''
+          flake8 --max-line-length 88 ${pySrc}
+          touch $out
+        '';
+
+      alejandra = pkgs.runCommand "alejandra" {} ''
+        ${pkgs.alejandra}/bin/alejandra --check ${nixSrc}
+        touch $out
+      '';
+
+      statix = pkgs.runCommand "statix" {} ''
+        ${pkgs.statix}/bin/statix check ${nixSrc}
+        touch $out
+      '';
+    };
+  };
+}
---
+++ module.nix
@@ -0,0 +1,148 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.services.brother_ql_web;
+in {
+  options.services.brother_ql_web = with lib; {
+    enable = mkEnableOption "brother_ql_web";
+
+    host = mkOption {
+      type = types.str;
+      default = "127.0.0.1";
+      description = "Address of the webserver.";
+    };
+
+    port = mkOption {
+      type = types.ints.u16;
+      default = 8013;
+      description = "Port of the web server.";
+    };
+
+    openFirewall = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Open the web server port in the firewall.";
+    };
+
+    printerModel = mkOption {
+      type = types.str;
+      description = "Printer model.";
+    };
+
+    printerUrl = mkOption {
+      type = types.str;
+      description = "Printer URL.";
+    };
+
+    logLevel = mkOption {
+      type = types.enum ["DEBUG" "INFO" "WARNING" "ERROR"];
+      default = "WARNING";
+      description = "Logging level";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [cfg.port];
+
+    systemd.services.brother_ql_web = let
+      configFile = pkgs.writeText "brother_ql_web_config.json" (builtins.toJSON {
+        SERVER = {
+          PORT = cfg.port;
+          HOST = cfg.host;
+          LOGLEVEL = cfg.logLevel;
+          ADDITIONAL_FONT_FOLDER = false;
+        };
+        PRINTER = {
+          MODEL = cfg.printerModel;
+          PRINTER = cfg.printerUrl;
+        };
+        LABEL = {
+          DEFAULT_SIZE = "12";
+          DEFAULT_ORIENTATION = "standard";
+          DEFAULT_FONT_SIZE = 70;
+          DEFAULT_FONTS = [
+            {
+              family = "Minion Pro";
+              style = "Semibold";
+            }
+            {
+              family = "Linux Libertine";
+              style = "Regular";
+            }
+            {
+              family = "DejaVu Serif";
+              style = "Book";
+            }
+          ];
+        };
+        WEBSITE = {
+          HTML_TITLE = "Label Designer";
+          PAGE_TITLE = "Brother QL Label Designer";
+          PAGE_HEADLINE = "Design your label and print it...";
+        };
+      });
+    in {
+      wantedBy = ["multi-user.target"];
+      after = ["network-online.target"];
+      description = "Brother QL WebUI";
+      path = with pkgs; [fontconfig];
+      environment = {
+        VIEWS_PATH = "${./views}";
+        STATIC_PATH = "${./static}";
+      };
+      serviceConfig = {
+        Type = "idle";
+        KillSignal = "SIGINT";
+        ExecStart = "${pkgs.python3Packages.brother_ql_web}/bin/brother_ql_web ${configFile}";
+        Restart = "on-failure";
+        RestartSec = 10;
+
+        # hardening
+        DynamicUser = true;
+        DevicePolicy = "closed";
+        CapabilityBoundingSet = "";
+        RestrictAddressFamilies = [
+          "AF_INET"
+          "AF_INET6"
+          "AF_NETLINK"
+          "AF_UNIX"
+        ];
+        DeviceAllow = [];
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateMounts = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
+        BindPaths = [];
+        MemoryDenyWriteExecute = true;
+        LockPersonality = true;
+        RemoveIPC = true;
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [
+          "~@debug"
+          "~@mount"
+          "~@privileged"
+          "~@resources"
+          "~@cpu-emulation"
+          "~@obsolete"
+        ];
+        ProtectProc = "invisible";
+        ProtectHostname = true;
+        ProcSubset = "pid";
+      };
+    };
+  };
+}
